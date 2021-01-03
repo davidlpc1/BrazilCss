2a3091d9df1ba78bf617ed53ecbce8ce3fb3db0a
Davi Lucas <davilucasdlcreator@gmail.com>
2021-01-03 12:56:57 -0300

    Border radius added

--- implement.js
+++ implement.js
@@ -1,14 +1,20 @@
 //Implementando o CSS automatizado
 const fs = require('fs');
-const contentFilePath = './implement.scss';
+const contentFilePath = './_implement.scss';
 let contentString = ``;
 
-const directions = { right:'direita',left:'esquerda',top:'topo',bottom:'abaixo' }
-for (let direction of [ 'right','left','top','bottom']){
-    for (let index = 1; index <= 100;index++){
-        contentString += `.p-${directions[direction]}-${index}px{padding-${direction}:${index}px !important;}`;
-        contentString += `.p-${directions[direction]}-${index}rem{padding-${direction}:${index}rem !important;}`;
-    }
+// const directions = { right:'direita',left:'esquerda',top:'topo',bottom:'abaixo' }
+// for (let direction of [ 'right','left','top','bottom']){
+//     for (let index = 1; index <= 100;index++){
+//         contentString += `.p-${directions[direction]}-${index}px{padding-${direction}:${index}px !important;}`;
+//         contentString += `.p-${directions[direction]}-${index}rem{padding-${direction}:${index}rem !important;}`;
+//     }
+// }
+
+for(let index = 0; index <= 100;index++){
+    contentString += `.raio_borda-${index}px,.border_radius-${index}px{border-radius:${index}px !important};`
+    contentString += `.raio_borda-${index}-relativo-altura,.border_radius-${index}-relativo-altura{border-radius:${index}vh !important};`
+    contentString += `.raio_borda-${index}-porcento,.border_radius-${index}-porcento{border-radius:${index}% !important};`
 }
 
 function writeFile(filePath,fileContent){
