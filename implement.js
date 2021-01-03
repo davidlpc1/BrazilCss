8341c8bfcc4e7caac402755fe499ee16b085134f
Davi Lucas <davilucasdlcreator@gmail.com>
2021-01-03 13:08:56 -0300

    Transition time added

--- implement.js
+++ implement.js
@@ -12,9 +12,8 @@ let contentString = ``;
 // }
 
 for(let index = 0; index <= 100;index++){
-    contentString += `.raio_borda-${index}px,.border_radius-${index}px{border-radius:${index}px !important};`
-    contentString += `.raio_borda-${index}-relativo-altura,.border_radius-${index}-relativo-altura{border-radius:${index}vh !important};`
-    contentString += `.raio_borda-${index}-porcento,.border_radius-${index}-porcento{border-radius:${index}% !important};`
+    contentString += `.transição-${index}ms,.transition-${index}ms,.transition-${index}miliseconds,.transição-${index}milisegundos{transition:${index}ms !important};`
+    contentString +=`.transição-${index}s,.transition-${index}s,.transition-${index}seconds,.transição-${index}segundos{transition:${index}s !important};`
 }
 
 function writeFile(filePath,fileContent){
@@ -29,7 +28,7 @@ writeFile(contentFilePath, contentString)
 const fileBuffer = readFile(contentFilePath);
 
 if(fileBuffer == contentString){
-    console.log({ status:200,description:"The content of file is equal to the expected" })
+    console.log({ status:201,description:"The content of file is equal to the expected" })
 }else{
     console.log({ status:400, description:"The content of file is not equal to the expected" });
 }
